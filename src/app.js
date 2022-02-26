f259a4b5ee8836bed3e5ad1a37f40b2c26a251ff
wilsonstevensCapy <stevenscard06@gmail.com>
2022-02-26 16:55:44 -0500

    agregando session

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
 
+/* agregando configuracion para la sesión*/
+const acceso = require('./middleware/acceso');
+const session = require('express-session');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +21,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes'));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method')); 
+
+/*usando session y configuracion para el login*/
+
+app.use(session({
+  secret : 'topSecret',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(acceso);
 
 
 // catch 404 and forward to error handler
