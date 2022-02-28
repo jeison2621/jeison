2855ee50f28c4d34349622c075e1879f422932ac
diegomduquec <diegomduquec@gmail.com>
2022-02-28 16:55:22 -0500

    Subiendo CRUD Usuarios detail, multer

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,17 +1,14 @@
 const model = require('../model')
 
 const userController = {
-    //FUNCIONA ///////////////
     adminUsers: (req, res, next) => {
         model.user.findAll().then(item => {
             res.render('user/users', { data: item })
         }).catch(err => next(err))
     },
-    //FUNCIONA ///////////////
     newUser: (req, res, next) => {
         res.render('user/newUser')
     },
-    //FUNCIONA ///////////////
     createUser: (req, res) => {
         model.user.create(
             {
@@ -19,7 +16,7 @@ const userController = {
                 lastname: req.body.apellidos,
                 email: req.body.email,
                 password: req.body.password,
-                avatar: req.body.image,
+                avatar: req.file ? req.file.filename: '',
                 roles_id: req.body.rol,
             }
         )
@@ -27,45 +24,34 @@ const userController = {
                 res.redirect('/admin/users')
             })
     },
-    //FUNCIONA ///////////////
     getEditUsers: (req, res, next) => {
         model.user.findOne(req.params.id)
             .then(function (item) {
                 res.render('user/editUser', { data: item })
             })
     },
-
-
-    detailUsers: (req, res) => {
+    detailUser: (req, res) => {
         model.user.findOne(req.params.id)
             .then(function (item) {
-                res.render('user/editUser', { data: item })
+                res.render('user/detailUser', { data: item })
             })
     },
     editUsers: (req, res, next) => {
-        model.product.update(
+        console.log(req.body);
+        model.user.update(
             {
-                name: req.body.name,
-                description: req.body.description,
-                image: req.file ? req.file.filename : '',
-                category: req.body.category,
-                amount: req.body.amount,
-                typeAmount: req.body.typeAmount,
-                price: req.body.price,
-                discount: req.body.discount
+                name: req.body.nombre,
+                lastname: req.body.apellidos,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.file ? req.file.filename: '',
+                roles_id: req.body.rol,
             }, req.params.id)
-
-
+            
             .then(function (item) {
-                res.redirect('/admin/products/')
-
-
-
+                res.redirect('/admin/users/')
             })
     },
-
-    
-    //FUNCION,A ///////////////
     borrarUser: (req, res) => {
         model.user.delete(req.params.id)
             .then(function (item) {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController')
+const upload = require('../middleware/multermidd')
 
 //Vista principal de usuarios
 router.get('/', userController.adminUsers)
 
+//Detalle usuario
+router.get('/:id', userController.detailUser)
+
 //Crear usuarios
 router.get('/newUser', userController.newUser)
-router.post('/newUser', userController.createUser)
+router.post('/newUser', upload.single('image'), userController.createUser)
 
 // Editar usuarios
 router.get('/editUser/:id',userController.getEditUsers)
-router.post('/editUser/:id',userController.editUsers)
+router.post('/editUser/:id', upload.single('image'), userController.editUsers)
 
 // Eliminar usuarios
 router.get('/borrar/:id',userController.borrarUser)
-router.get('/:id', userController.detailUsers)
 
 module.exports = router
