e1a159a2028e2019cbedf2349adba364d2bbecff
wilsonstevensCapy <stevenscard06@gmail.com>
2022-03-05 22:46:05 -0500

    registro usuarios funcinando

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { navigationController} = require('../controller');
+const upload = require('../middleware/multermidd')
 
 
 router.get('/',navigationController.getHome)
 router.get('/admin',navigationController.getAdmin)
 
 router.get('/register',navigationController.register)
+router.post('/register', upload.single('image'), navigationController.guardar)
 
 
 router.get('/login',navigationController.login)
