3008f53c68f7658511e4e96362adb5454fdc622c
jeison2621 <andres.moyano@misena.edu.co>
2022-03-05 20:33:55 -0500

    cambios de new user

--- src/public/js/valitationsUsers.js
+++ src/public/js/valitationsUsers.js
@@ -11,8 +11,8 @@ window.addEventListener('load', function () {
             errores.push("Debe completar el campo nombre(s)")
         }
 
-        let lastname = document.querySelector('#lastname')       
-        if (lastname.value == "") {
+        let description = document.querySelector('#lastname')       
+        if (description.value == "") {
             errores.push("Debe completar el campo apellidos")
         }
 
