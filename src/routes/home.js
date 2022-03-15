4e45f41c6933301408e8d4cc1ec9ec2a969fec4b
wilsonstevensCapy <stevenscard06@gmail.com>
2022-03-14 19:05:26 -0500

    cambio

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,6 +4,7 @@ const { navigationController, apiController } = require('../controller');
 const upload = require('../middleware/multermidd')
 const { validationsUsers } = require('../middleware')
 const { acceso } = require('../middleware')
+const {validationLogin} = require ('../middleware/valitations')
 
 
 
@@ -27,7 +28,7 @@ router.get('/register',navigationController.register)
 router.post('/register', upload.single('image'),validationsUsers,navigationController.guardar);
 
 router.get('/login',navigationController.login); 
-router.post('/login', acceso,  navigationController.ingresar);
+router.post('/login', acceso, validationLogin,  navigationController.ingresar);
 
 router.get('/logout',navigationController.logout); 
 
