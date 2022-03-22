77b2b97b3483bd3844c574b163a185e247a5b524
jeison2621 <andres.moyano@misena.edu.co>
2022-03-22 15:24:33 -0500

    carrusel terminado y css mejorado

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,1 +1,34 @@
 console.log("entro")
+
+
+
+
+const carrusel = document.querySelector(".carrusel-items");
+let maxscrollLeft = carrusel.scrollWidth-carrusel.clientWidth
+let intervalo = null;
+let step = 112;
+const start= ()=>{
+    intervalo = setInterval(function(params){
+        carrusel.scrollLeft = carrusel.scrollLeft + step
+        if(carrusel.scrollLeft === maxscrollLeft){
+            step = -4
+        }else if(carrusel.scrollLeft===0){
+            step = step *-1;
+        }
+    },100)
+
+};
+
+
+const stop= ()=>{
+    clearInterval(intervalo)
+};
+carrusel.addEventListener("mouseover", ()=>{
+    stop()
+})
+
+carrusel.addEventListener("mouseout",()=>{
+    start()
+})
+
+start();
